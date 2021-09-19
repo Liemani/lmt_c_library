eb58ee3638291440de7e8e101f9293c18cb3aff0
Liemani <parkjh2768@naver.com>
2021-09-19 14:17:39 +0900

    Create lmt_unsafe_strdup.c

--- content/lmt_unsafe/lmt_unsafe_strdup.c
+++ content/lmt_unsafe/lmt_unsafe_strdup.c
@@ -1,22 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lmt_unsafe.h                                       :+:      :+:    :+:   */
+/*   lmt_unsafe_strdup.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jeonpark <jeonpark@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/14 22:22:05 by jeonpark          #+#    #+#             */
-/*   Updated: 2021/09/19 14:13:12 by jeonpark         ###   ########.fr       */
+/*   Created: 2021/09/19 14:13:27 by jeonpark          #+#    #+#             */
+/*   Updated: 2021/09/19 14:13:49 by jeonpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LMT_UNSAFE_H
-# define LMT_UNSAFE_H
+#include <stddef.h>
+#include "lmt_unsafe.h"
+#include "lmt_alloc.h"
 
-# include <stddef.h>
+void	*lmt_unsafe_strdup(const char *string)
+{
+	size_t	count;
+	char	*string_dup;
 
-size_t	lmt_unsafe_strlen(const char *string);
-void	*lmt_unsafe_memcpy(void *p_write, const void *p_read, size_t size);
-void	*lmt_unsafe_strdup(const char *string);
-
-#endif
+	count = lmt_unsafe_strlen(string) + 1;
+	string_dup = lmt_alloc(sizeof(*string_dup) * count);
+	lmt_unsafe_memcpy(string_dup, string, count);
+	return (string_dup);
+}
