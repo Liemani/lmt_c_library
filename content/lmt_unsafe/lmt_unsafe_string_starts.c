943b368359f502465456af0a32b1268c2b3da501
Liemani <parkjh2768@naver.com>
2021-09-19 16:27:53 +0900

    Create lmt_unsafe_string_starts()

--- content/lmt_unsafe/lmt_unsafe_string_starts.c
+++ content/lmt_unsafe/lmt_unsafe_string_starts.c
@@ -1,23 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lmt_unsafe.h                                       :+:      :+:    :+:   */
+/*   lmt_unsafe_strcmp.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jeonpark <jeonpark@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/14 22:22:05 by jeonpark          #+#    #+#             */
-/*   Updated: 2021/09/19 17:17:22 by jeonpark         ###   ########.fr       */
+/*   Created: 2021/09/19 16:26:35 by jeonpark          #+#    #+#             */
+/*   Updated: 2021/09/19 17:15:43 by jeonpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LMT_UNSAFE_H
-# define LMT_UNSAFE_H
+#include "lmt_unsafe.h"
+#include "lmt_constant.h"
 
-# include <stddef.h>
-
-size_t	lmt_unsafe_strlen(const char *string);
-int		lmt_unsafe_string_starts(char *string, char *word);
-void	*lmt_unsafe_memcpy(void *p_write, const void *p_read, size_t size);
-void	*lmt_unsafe_strdup(const char *string);
-
-#endif
+int	lmt_unsafe_string_starts(char *string, char *word)
+{
+	while (*word != '\0')
+		if (*string++ != *word++)
+			return (FALSE);
+	return (TRUE);
+}
