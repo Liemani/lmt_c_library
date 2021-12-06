a43d59928177019701915c36d57474f19702a1b3
Liemani <parkjh2768@naver.com>
2021-12-06 20:52:45 +0900

    Update lmt_strcpy() to copy '\0'

--- contents/lmt_primitive_type/lmt_strcpy.c
+++ contents/lmt_primitive_type/lmt_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: jeonpark <jeonpark@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 20:13:27 by jeonpark          #+#    #+#             */
-/*   Updated: 2021/11/23 11:51:39 by jeonpark         ###   ########.fr       */
+/*   Updated: 2021/12/06 20:51:53 by jeonpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ char	*lmt_strcpy(char *target, const char *source)
 		return (target);
 	while (*source != '\0')
 		*target++ = *source++;
+	*target = '\0';
 	return (target);
 }
